084f0232d3324fdf1ff6933070aa92998aad1fb6
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:39:15 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan11formating/IF1101108015Latihan11formating.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan11formating;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program formating angka keluaran
+ */
+public class IF1101108015Latihan11formating {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n" , i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f :%f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
